c559905d590c483e165e71142b37591fea4dbc57
mehdi1 <mehdi3_ins@hotmail.fr>
2013-11-05 23:47:20 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuvoApp::Application.config.secret_token = 'ec64683d4dbf2055dd427c2ee3ac13f3a42812f6e4a9a9e5646d392641c839246909080ad705d97f3c647e9e49a02b2f546ca61c96c02244682759ab57ecd337'
